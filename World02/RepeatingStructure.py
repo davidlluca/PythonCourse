554c6b30d128d1d361ef14f38bad7b5f1358d3cf
David Luca <davidlluca085@gmail.com>
2023-06-13 10:51:27 -0300

    Doing exercises 46, 47, 48, 49

--- World02/RepeatingStructure.py
+++ World02/RepeatingStructure.py
@@ -2,23 +2,71 @@
 # for 'variable_name' in range():
 # (0,0,0) '1' - begin | '2' - end | '3' - jump
 
-begin = int(input('Write the begin: '));
-end = int(input('Write the end: '));
-jump = int(input('Write the jump: '));
 
-for test in range(begin, end + 1, jump):
-    print(test);
+# # Test 1
+# begin = int(input('Write the begin: '));
+# end = int(input('Write the end: '));
+# jump = int(input('Write the jump: '));
+
+# for test in range(begin, end + 1, jump):
+#     print(test);
+
+
+# # Test 2   
+# from random import randint;
+
+# computerNumber = randint(0,5);
+
+# for guess in range(0,3):
+#     personNumber = int(input('Try guess the computer number: '));
+    
+#     if personNumber == computerNumber:
+#         print('You are right!');
+#     else:
+#         print('You lose!');
 
 
 
+# # Challenger 46 ------------------------------------------------------------------------------------------------
+# from time import sleep;
 
-# Challenger 46 ------------------------------------------------------------------------------------------------
+# for count in range(10,0, -1):
+#     print('{0}!'.format(count));
+#     sleep(1);
+    
+# print('Happy New Year!');
 
-# Challenger 47 -------------------------------------------------------------------------------------------------
 
-# Challenger 48 -------------------------------------------------------------------------------------------------
+# # Challenger 47 -------------------------------------------------------------------------------------------------
+# for evenNumber in range(1,51):
+#     if evenNumber % 2 == 0:
+#         print(evenNumber);
+        
+# print('The end!');
 
-# Challenger 49 -------------------------------------------------------------------------------------------------
+
+# # Challenger 48 -------------------------------------------------------------------------------------------------
+# sum = 0;
+# count = 0;
+
+# for numbersCalc in range(1,501, 2):
+#     if numbersCalc % 3 == 0:
+#         sum = sum + numbersCalc;
+#         count = count + 1;
+        
+# print('The sum between the {0} numbers is {1}'.format(count, sum));
+
+
+# # Challenger 49 -------------------------------------------------------------------------------------------------
+# count = 0;
+# result = 0;
+# number = int(input('Write a number: '));
+
+# for numbersTab in range(0,10):
+#     count = count + 1;
+#     result = number * count;
+#     print('{0}x{1} = {2}'.format(number, count, result));
+
 
 # Challenger 50 -------------------------------------------------------------------------------------------------
 
@@ -32,4 +80,4 @@ for test in range(begin, end + 1, jump):
 
 # Challenger 55 -------------------------------------------------------------------------------------------------
 
-# Challenger 56 -------------------------------------------------------------------------------------------------
+# Challenger 56 -------------------------------------------------------------------------------------------------;
